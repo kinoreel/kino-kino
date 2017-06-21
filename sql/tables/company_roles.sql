86c4100acb08d74cbee597617fdb53fc18fb6c4a
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-06-21 22:41:07 +0100

    Added country codes, removed person_roles and company_roles table

--- sql/tables/company_roles.sql
+++
@@ -1,5 +0,0 @@
-create table kino.company_roles
-   ( role varchar(250) not null
-   , tstamp date not null  default CURRENT_DATE
-   , PRIMARY KEY (role)
-);
--- sql/tables/person_roles.sql
+++
@@ -1,5 +0,0 @@
-create table kino.person_roles
-   ( role varchar(250) not null
-   , tstamp date not null default CURRENT_DATE
-   , PRIMARY KEY (role)
-);
