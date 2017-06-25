59dd7cdfd63d3e5e6580056b4ddb3fe5f7c8c1e0
unruffled-nightingale <unruffled.nightingale@gmail.com>
2017-06-25 23:08:57 +0100

    Corrected primary key

--- sql/tables/movies2awards.sql
+++ sql/tables/movies2awards.sql
@@ -2,11 +2,10 @@ create table kino.movies2awards
    ( imdb_id varchar(10) not null
    , imdb_event_id varchar(10) not null
    , award varchar(10) not null
-   , position varchar(1) check (won in ('N', 'W'))
-   , year date not null 
+   , position varchar(1) check (position in ('N', 'W'))
+   , year date not null
    , tstamp date not null
-   , FOREIGN KEY (imdb_id) references kino.movies(movie_id)
-   , FOREIGN KEY (imdb_event_id) references kino.awards(imdb_event_id)
-   , FOREIGN KEY (award) references kino.awards(award)
+   , FOREIGN KEY (imdb_id) references kino.movies(imdb_id)
+   , FOREIGN KEY (imdb_event_id, award) references kino.awards(imdb_event_id, award)
    , UNIQUE (imdb_id, award, year, position)
 );
