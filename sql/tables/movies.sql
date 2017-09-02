3cb2fd710d4aadb7acd9fc070c704da0d816ae93
ted <tedjohanssondeveloper@gmail.com>
2017-09-02 14:55:46 +0100

    Added column in the movies table

--- sql/tables/movies.sql
+++ sql/tables/movies.sql
@@ -1,6 +1,7 @@
 create table kino.movies 
     ( imdb_id varchar(10) not null
     , title varchar(1000) not null
+    , overview varchar(1000) not null
     , runtime varchar(100) not null
     , rated varchar(15) not null
     , released varchar(15) not null
