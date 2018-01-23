05f10d196e41c9a159d9aae4769daf3dc5d4d774
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-01-23 18:12:44 +0000

    Changed column type

--- sql/tables/movies2ratings.sql
+++ sql/tables/movies2ratings.sql
@@ -1,7 +1,7 @@
 create table kino.movies2ratings
    ( imdb_id varchar(10) not null
    , source varchar(100) not null
-   , rating varchar(100) not null
+   , rating real not null
    , tstamp date not null default CURRENT_DATE
    , PRIMARY KEY (imdb_id, source)
    , FOREIGN KEY (imdb_id) references kino.movies(imdb_id) on delete cascade
--- sql/tables/movies2trailers.sql
+++ sql/tables/movies2trailers.sql
@@ -1,8 +1,17 @@
 create table kino.movies2trailers
    ( imdb_id varchar(10)
-   , url varchar(400)
+   , video_id varchar(400)
+   , title varchar(400)
+   , channel_id varchar(400)
+   , channel_title varchar(400)
+   , definition varchar(2)
+   , duration int
+   , view_count int
+   , like_count int
+   , dislike_count int
+   , comment_count int
+   , published_at date
    , tstamp date default current_date
-   , PRIMARY KEY (imdb_id, url)
+   , PRIMARY KEY (imdb_id)
    , FOREIGN KEY (imdb_id) references kino.movies(imdb_id) on delete cascade
    );
-              
