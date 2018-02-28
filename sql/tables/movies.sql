6c1fede98c5c5c556579f64bdb7869d1b6e6e23d
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-02-28 18:10:42 +0000

    Removed columns we have no data for

--- sql/tables/movies.sql
+++ sql/tables/movies.sql
@@ -1,10 +1,10 @@
 create table kino.movies 
     ( imdb_id varchar(10) not null
     , title varchar(1000) not null
-    , plot text not null
     , runtime varchar(100) not null
     , rated varchar(15) not null
     , released date not null
+    , plot text not null
     , orig_language varchar(1000) not null
     , tstamp date not null  default CURRENT_DATE
     , PRIMARY KEY (imdb_id)
