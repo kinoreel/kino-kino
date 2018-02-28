2fc00c64e7638c6b7d294ccfc6aa2aacea8d7dca
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-02-28 18:16:49 +0000

    changing runtime to int

--- sql/tables/movies.sql
+++ sql/tables/movies.sql
@@ -1,7 +1,7 @@
 create table kino.movies 
     ( imdb_id varchar(10) not null
     , title varchar(1000) not null
-    , runtime varchar(100) not null
+    , runtime int not null
     , rated varchar(15) not null
     , released date not null
     , plot text not null
