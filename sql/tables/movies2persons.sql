20d77093f5800d43209a3c65810a78aaac4635b1
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-02-28 18:25:08 +0000

    adding cast_order column

--- sql/tables/movies2persons.sql
+++ sql/tables/movies2persons.sql
@@ -2,6 +2,7 @@ create table kino.movies2persons
    ( imdb_id varchar(10) not null
    , person_id integer
    , role varchar(250) not null
+   , cast_order int
    , tstamp date not null  default CURRENT_DATE
    , PRIMARY KEY (imdb_id, person_id, role)
    , FOREIGN KEY (imdb_id) references kino.movies(imdb_id) on delete cascade
