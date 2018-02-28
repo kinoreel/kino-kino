2767d062a90438dc1de833f4dfb4a83e7c6d316f
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-02-28 18:21:31 +0000

    changing value to int

--- sql/tables/movies2numbers.sql
+++ sql/tables/movies2numbers.sql
@@ -1,7 +1,7 @@
 create table kino.movies2numbers
    ( imdb_id varchar(10) not null
    , type varchar(250) not null
-   , value real not null
+   , value int not null
    , tstamp date not null default CURRENT_DATE
    , PRIMARY KEY (imdb_id, type)
    , FOREIGN KEY (imdb_id) references kino.movies(imdb_id) on delete cascade
